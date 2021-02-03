9894dde3f8dbc8321ed9c8729f5a8fa222c1d695
Artem <artem2000vlas@mail.ru>
2021-02-03 14:46:54 +0300

    replace issue id to corner of card

--- src/content-scripts/gitlab-script.js
+++ src/content-scripts/gitlab-script.js
@@ -12,12 +12,19 @@ gitlabService.getAllIssues()
 setTimeout(() => {
     var elements = document.getElementsByClassName("board-card");
     for (var i = 0; i < elements.length; ++i) {
-        var wrapper = document.createElement('qoollab-card');
-        elements[i].parentNode.insertBefore(wrapper, elements[i]);
-        wrapper.appendChild(elements[i]);
-        var children = elements[i].getElementsByClassName("board-card-number");
-        var strIssueId = children[0].innerHTML.toString().replace(/[^\d]/g, ''); // удаляем из innerHTML всё кроме цифр
+        var qoollabCard = document.createElement('qoollab-card');
+        elements[i].parentNode.insertBefore(qoollabCard, elements[i]);
+        qoollabCard.appendChild(elements[i]);
+        var oldIssueIdElement = elements[i].getElementsByClassName("board-card-number")[0];
+        var strIssueId = oldIssueIdElement.innerHTML.toString().replace(/[^\d]/g, '');    // удаляем из innerHTML всё кроме цифр
         var issueId = Number.parseInt(strIssueId);
-        wrapper.setAttribute("issue-id", issueId)
+        qoollabCard.setAttribute("issue-id", issueId);                                // сохранили id isuue как аттрибут <qoollab-card>
+        oldIssueIdElement.parentElement.removeChild(oldIssueIdElement);                          // удалили id isuue
+        var cardHeader = qoollabCard.querySelector("li > div > div:nth-child(1)");
+        var issueIdElement = document.createElement('div');
+        issueIdElement.textContent = "#" + issueId;
+        issueIdElement.style = "color: rgba(146, 146, 146, 1); font-size: small; padding-left: 10px";
+        cardHeader.appendChild(issueIdElement);
+        cardHeader.setAttribute("style", "justify-content: space-between");
     }
-}, 10000);
+}, 2000);
