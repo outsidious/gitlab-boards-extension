ac4ef55cda7144094b06d24eb0f145634e3d57c2
Artem <artem2000vlas@mail.ru>
2021-03-28 13:38:06 +0300

    fix icon in milestone component!

--- src/content-scripts/components/Milestone.vue
+++ src/content-scripts/components/Milestone.vue
@@ -1,13 +1,22 @@
 <template>
     <div class="milestone-container">
-        <img src="../../assets/sprite_icons/clock.svg" alt="clock">
+        <img alt="clock-icon" :src="clockIconUrl"/>
         {{ due_date }}
     </div>
 </template>
 
 <script>
+import "../../assets/sprite_icons/clock.svg";
 export default {
     props: ["due_date"],
+    data() {
+        return {
+            clockIconUrl: "",
+        };
+    },
+    mounted() {
+        this.clockIconUrl = chrome.extension.getURL(require(`../../assets/sprite_icons/clock.svg`))
+    },
 };
 </script>
 
