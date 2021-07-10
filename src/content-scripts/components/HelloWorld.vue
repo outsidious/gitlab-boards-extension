83f9a47ab674c18e674638a610ee9b94abe3d812
Artem <artem2000vlas@mail.ru>
2021-07-10 20:26:21 +0300

    return HelloWorld.vue

---
+++ src/content-scripts/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="popup-container">
+        <a class="popup-elem" v-on:click="openOptions()"> Options page </a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HelloWorld",
+    methods: {
+        openOptions() {
+			chrome.tabs.create({'url': "/options.html" } )
+        },
+    },
+};
+</script>
+
+<style scoped>
+.popup-elem {
+    color: black;
+    font-weight: 600;
+	font-size: 18pt;
+}
+
+.popup-container {
+	width: inherit;
+	height: inherit;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+</style>
